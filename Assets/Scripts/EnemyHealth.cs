f8e6cb76a32449678eac623424930d960efb1ed2
Omar Lawrence <mromarlawrence@gmail.com>
2017-11-28 19:59:44 -0500

    finished enemy characters

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyHealth : MonoBehaviour {
+
+	public int startingHealth = 100;
+    public int currentHealth;
+    public float sinkSpeed = 2.5f;              // The speed at which the enemy sinks through the floor when dead.
+    public int scoreValue = 10;                 // The amount added to the player's score when the enemy dies.
+    public AudioClip deathClip;                 // The sound to play when the enemy dies.
+
+
+    Animator anim;                             
+    AudioSource enemyAudio;                     
+    ParticleSystem hitParticles;               
+    CapsuleCollider capsuleCollider;
+    bool isDead;                           
+    bool isSinking; 
+
+	void Awake (){
+        anim = GetComponent <Animator> ();
+        enemyAudio = GetComponent <AudioSource> ();
+        hitParticles = GetComponentInChildren <ParticleSystem> ();
+        capsuleCollider = GetComponent <CapsuleCollider> ();
+
+        currentHealth = startingHealth;
+    }
+	void Update (){
+        if(isSinking)
+        {
+            // ... move the enemy down by the sinkSpeed per second.
+            transform.Translate (-Vector3.up * sinkSpeed * Time.deltaTime);
+        }
+    }
+
+	public void TakeDamage (int amount, Vector3 hitPoint){
+        if(isDead)
+            // ... no need to take damage so exit the function.
+            return;
+
+        // Play the hurt sound effect.
+        enemyAudio.Play ();
+
+        // Reduce the current health by the amount of damage sustained.
+        currentHealth -= amount;
+            
+        // Set the position of the particle system to where the hit was sustained.
+        hitParticles.transform.position = hitPoint;
+
+        // And play the particles.
+        hitParticles.Play();
+
+
+        if(currentHealth <= 0)
+        {
+            Death ();
+        }
+    }
+
+	void Death (){
+        // The enemy is dead.
+        isDead = true;
+
+        // Turn the collider into a trigger so shots can pass through it.
+        capsuleCollider.isTrigger = true;
+
+        // Tell the animator that the enemy is dead.
+        anim.SetTrigger ("Dead");
+
+        // Change the audio clip of the audio source to the death clip and play it (this will stop the hurt clip playing).
+        enemyAudio.clip = deathClip;
+        enemyAudio.Play ();
+    }
+
+	public void StartSinking ()
+    {
+        // Find and disable the Nav Mesh Agent.
+        GetComponent <NavMeshAgent> ().enabled = false;
+
+        // Find the rigidbody component and make it kinematic (since we use Translate to sink the enemy).
+        GetComponent <Rigidbody> ().isKinematic = true;
+
+        // The enemy should no sink.
+        isSinking = true;
+
+        // Increase the score by the enemy's score value.
+        //ScoreManager.score += scoreValue;
+
+        // After 2 seconds destory the enemy.
+        Destroy (gameObject, 2f);
+    }
+
+}
---
+++ Assets/Scripts/ZombieScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ZombieScript : MonoBehaviour {
+	public float m_moveSpeed = 0.0f;
+	private Transform player;
+	//public PlayerHealth playerHealth;
+	EnemyHealth enemyHealth;
+	EnemyAttack enemyAttack;
+	
+
+	private NavMeshAgent nav;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+		//playerHealth = player.GetComponent <PlayerHealth>();
+		enemyHealth = GetComponent <EnemyHealth>();
+		enemyAttack = GetComponent <EnemyAttack>();
+		nav = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		nav.speed = enemyAttack.runSpeed;
+		//Debug.Log(nav.speed);
+		if(enemyHealth.currentHealth > 0 /*e&& playerHealth.currentHealth > 0*/){
+			nav.SetDestination(player.position);
+		} else{
+			nav.enabled = false;
+		}
+
+	}
+}
