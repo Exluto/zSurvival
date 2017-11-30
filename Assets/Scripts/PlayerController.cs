a10e2ff0de1e7372e412aa5170b7d7ea9dd01db0
Tom Sauer <tom@exluto.com>
2017-11-29 20:58:42 -0500

    adding player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,16 +7,35 @@ public class PlayerController : MonoBehaviour {
 	private float m_speed = 10.0f;
 	private float m_Sensitivity = 10.0f;
 	CharacterController m_player;
+	public GameObject m_eyes;
+
+	float m_moveFB;
+	float m_moveRL;
+	float m_rotationX;
+	float m_rotationY;
 
 	
 	// Use this for initialization
 	void Start () {
-		GetComponent<CharacterController>();
+		m_player = GetComponent<CharacterController>();
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
+		m_moveRL =  Input.GetAxis("Horizontal") * m_speed;
+		m_moveFB = Input.GetAxis("Vertical") * m_speed;
+
+		m_rotationX = Input.GetAxis("Mouse X") * m_Sensitivity;
+		m_rotationY = Input.GetAxis("Mouse Y") * m_Sensitivity;
+
+		Vector3 movement = new Vector3(m_moveRL, 0, m_moveFB);
+		transform.Rotate(0, m_rotationX, 0);
+		transform.Rotate(-m_rotationY, 0, 0 );
+
+		movement = transform.rotation * movement;
+		m_player.Move (movement * Time.deltaTime);
+
 		
 	}
 }
