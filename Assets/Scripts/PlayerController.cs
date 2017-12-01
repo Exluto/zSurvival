bb961059c5f0d9bb68781a37bd6faa97ad7179e5
Tom Sauer <tom@exluto.com>
2017-11-30 21:08:32 -0500

    Player Movement Added
    Added player movement with animations that are buggy

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	private float m_speed = 10.0f;
+	private float m_Velocity = 10.0f;
 	private float m_Sensitivity = 10.0f;
 	CharacterController m_player;
 	public GameObject m_eyes;
+	private Animator m_Anim;
 
 	float m_moveFB;
 	float m_moveRL;
@@ -18,22 +19,31 @@ public class PlayerController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		m_player = GetComponent<CharacterController>();
+		m_Anim = GetComponent<Animator>();
 		
+	}
+
+	void Update() {
+		Movement();
 	}
 	
 	// Update is called once per frame
-	void FixedUpdate () {
-		m_moveRL =  Input.GetAxis("Horizontal") * m_speed;
-		m_moveFB = Input.GetAxis("Vertical") * m_speed;
+	void Movement() {
+		m_moveRL =  Input.GetAxis("Horizontal") * m_Velocity;
+		m_moveFB = Input.GetAxis("Vertical") * m_Velocity;
+		m_Anim.SetFloat("m_speed", m_Velocity);
 
 		m_rotationX = Input.GetAxis("Mouse X") * m_Sensitivity;
-		m_rotationY = Input.GetAxis("Mouse Y") * m_Sensitivity;
+		m_rotationY -= Input.GetAxis("Mouse Y") * m_Sensitivity;
 
 		Vector3 movement = new Vector3(m_moveRL, 0, m_moveFB);
+
 		transform.Rotate(0, m_rotationX, 0);
-		transform.Rotate(-m_rotationY, 0, 0 );
 
 		movement = transform.rotation * movement;
+
+		m_eyes.transform.localRotation = Quaternion.Euler(m_rotationY, 0, 0);
+
 		m_player.Move (movement * Time.deltaTime);
 
 		
